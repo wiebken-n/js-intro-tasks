3063b5584e3f1632303037a96cfc5afa3c20998c
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-05-01 00:47:46 +0200

    Feat: provides solutions for js-task 02

---
+++ 02-operators-data-types/script.js
@@ -0,0 +1,25 @@
+console.log("W" + "i" + "e" + "b" + "k" + "e" + "n");
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 * 2 > 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" != 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+// Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+
+console.log(25 / 5 === 50 / 10);
